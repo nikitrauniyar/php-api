4cc95126f58f6cc3165fab7d0839c16df34cca08
Nikit Rauniyar <rauniyarnikit@gmail.com>
2022-04-11 20:18:04 +1000

    API Printed in decreasing alphabetical order

--- index.php
+++ index.php
@@ -1,27 +1,30 @@
 <?php
 
 
-$url = "https://api.publicapis.org/entries";
-$json = file_get_contents($url);
-$json_data = json_decode($json, true);
-$entries= $json_data['entries'];
+$url = "https://api.publicapis.org/entries"; // declaring url
+$json = file_get_contents($url);             //getting json from api
+$json_data = json_decode($json, true);       // decoding json
+$entries= $json_data['entries'];               // Holding entries data which is an array
 
+$category=$argv[1]; // tapping argument category
+$limit=$argv[2]; // tapping argument limit
+
+$api_array=array(); // declaring an empty array just to hold APIs from data recieved
+
+
+// making a new array of APIs looping through each entry
 foreach($entries as $entry){
-    echo $entry['API']."\n";
+    array_push($api_array, $entry['API']);
+}
+
+//sorting the new API array alphabetically
+sort($api_array);
+
+foreach($api_array as $api){
+    echo $api."\n";
 }
 
 
-// $ch = curl_init();
-// // IMPORTANT: the below line is a security risk, read https://paragonie.com/blog/2017/10/certainty-automated-cacert-pem-management-for-php-software
-// // in most cases, you should set it to true
-// curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-// curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-// curl_setopt($ch, CURLOPT_URL, $url);
-// $result = curl_exec($ch);
-// curl_close($ch);
-
-// $obj = json_decode($result);
-// echo $obj->count;
 
 ?>
 
